3fd16c1a317dd20c23257ba675e11129e0c902c4
Cheltor <rchelton@umd.edu>
2018-08-20 21:36:03 -0400

    add-businesses

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :businesses
   root 'staticpages#dashboard'
   get 'dashboard' => 'staticpages#dashboard'
   get 'myposts' => 'posts#myposts'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817015516) do
+ActiveRecord::Schema.define(version: 20180821010838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,28 @@ ActiveRecord::Schema.define(version: 20170817015516) do
     t.integer  "user_id"
     t.decimal  "price",      precision: 8, scale: 2
     t.index ["user_id"], name: "index_books_on_user_id", using: :btree
+  end
+
+  create_table "businesses", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["confirmation_token"], name: "index_businesses_on_confirmation_token", unique: true, using: :btree
+    t.index ["email"], name: "index_businesses_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_businesses_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "comments", force: :cascade do |t|
