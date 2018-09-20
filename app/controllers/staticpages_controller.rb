0c998495967f86121b6bcef7d98dd300df97b39a
Cheltor <rchelton@umd.edu>
2018-09-20 12:24:11 -0400

    bizdash

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -12,6 +12,11 @@ class StaticpagesController < ApplicationController
       @myposts = Post.all.where(user: current_user)
       @comments = Comment.all
       @mycomments = Comment.all.where(user: current_user)
+      #@rewards = Reward.all
+      #@myrewards = Reward.all.where(user: current_user)
+    elsif business_signed_in?
+      @rewards = Reward.all
+      @myrewards = Reward.all.where(business: current_business )
     else
       @search = Post.ransack(params[:q])
       @posts = @search.result.includes(:comments)
